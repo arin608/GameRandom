3308ae8f6c45e977a40facf6b0d2b89eee823722
y0enii <y0enii2@naver.com>
2024-04-16 15:10:39 +0900

    에러나서 안 돌아가서 원상복귀

--- GameRandom/src/controller/GameController.java
+++ GameRandom/src/controller/GameController.java
@@ -17,46 +17,36 @@ public class GameController {
     }
     
     public void startProgram() {
-        boolean running = true;
-
-        while (running) {
-            int choice = consoleView.UserInformationSelection();
-
-            switch (choice) {
-                case 1:
-                	String newUsername = consoleView.promptUsername(); // 새로운 닉네임 입력
-                    gameScore.addUser(newUsername);
-                    break;
-                case 2:
-                	consoleView.displayMessage("수정할 유저의 닉네임을 입력하세요: ");
-                    String existingUsername = consoleView.promptUsername(); // 수정할 닉네임 입력
-                    if (gameScore.hasUser(existingUsername)) {
-                        consoleView.displayMessage("새로운 닉네임을 입력하세요: ");
-                        newUsername = consoleView.promptUsername(); // 새로운 닉네임 입력
-                        gameScore.updateUser(existingUsername, newUsername);
-                    } else {
-                        consoleView.displayMessage("해당 유저를 찾을 수 없습니다.");
-                    }
-                    break;
-                case 3:
-                	consoleView.displayMessage("삭제할 유저의 닉네임을 입력하세요: ");
-                    String usernameToDelete = consoleView.promptUsername(); // 삭제할 닉네임 입력
-                    gameScore.removeUser(usernameToDelete);
-                    break;
-                case 4:
-                    gameScore.displayAllUsers(consoleView);
-                    break;
-                case 5:
-                    consoleView.displayMessage("프로그램을 종료합니다.");
-                    running = false;
-                    break;
-                default:
-                    consoleView.displayMessage("잘못된 선택입니다. 다시 선택해주세요.");
-                    break;
-            }
-        }
+    	boolean running = true;
+    	
+    	while (running) {
+    		int choice = consoleView.UserInformationSelection();
+    		
+    		switch (choice) {
+    		case 1:
+    			consoleView.promptUsername();
+    			startGame();
+    			break;
+    		case 2:
+    			consoleView.promptUsername();
+    			startGame();
+    			break;
+    		case 3:
+    			consoleView.promptUsername();
+    			break;
+    		case 4:
+    			gameScore.displayAllUsers(consoleView);
+    			break;
+    		case 5:
+    			consoleView.displayMessage("프로그램을 종료합니다.");
+    			running = false;
+    			break;
+    		default:
+    			consoleView.displayMessage("잘못된 선택입니다. 다시 선택해주세요.");
+    			break;
+    		}
+    	}
     }
-
 
     public void startGame() {
         boolean running = true;
